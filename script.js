0bef2401f3cfaf7e01d265863df7b756f59e65bc
ShivamP-97 <shivampathak19972@gmail.com>
2026-01-28 22:40:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
 //your JS code here. If required.
+const https = require('https');
+
+const url = 'https://jsonplaceholder.typicode.com/todos/1';
+
+https.get(url, (res) => {
+  let data = '';
+
+  // Collect response data
+  res.on('data', chunk => {
+    data += chunk;
+  });
+
+  // When response is complete
+  res.on('end', () => {
+    const parsedData = JSON.parse(data);
+
+    console.log('API Response:');
+    console.log(`ID: ${parsedData.id}`);
+    console.log(`Title: ${parsedData.title}`);
+    console.log(`Completed: ${parsedData.completed}`);
+  });
+
+}).on('error', (err) => {
+  console.error('Error making request:', err.message);
+});
